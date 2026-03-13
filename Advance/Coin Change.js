b730a6f93ae55c56bd7e38f1ef7a13e5cb7f039a
Nawin chander <nawinnapzy289@gmail.com>
2026-03-13 06:29:56 +0530

    add the code

---
+++ Advance/Coin Change.js
@@ -0,0 +1,30 @@
+// coins = [1,2,5]
+// amount = 11
+
+// Output = 3
+// (5 + 5 + 1)
+
+
+
+function coinChange(coins, amount) {
+
+    let dp = new Array(amount + 1).fill(Infinity)
+
+    dp[0] = 0
+
+    for (let coin of coins) {
+
+        for (let i = coin; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - coin] + 1)
+        }
+
+    }
+
+    return dp[amount] === Infinity ? -1 : dp[amount]
+}
+
+console.log(coinChange([1,2,5], 11))
+
+
+
+//// O(amount * number_of_coins)

93c5ab93ddefbc667ae6eafa4159d4a0f5050f9d
Nawin chander <nawinnapzy289@gmail.com>
2026-03-13 06:20:22 +0530

    add code

--- script.js
+++ script.js
@@ -52,3 +52,56 @@ function fib(n){
 console.log(fib(6))
 
 /// /// Time Complexity & space complexity 0(n)
+
+
+////  space opitimized DP
+
+function fib(n){
+    let a = 0
+    let b = 1
+
+    for(let i = 2; i <= n; i++){
+        let temp = a + b
+        a = b
+        b = temp
+    }
+
+    return n === 0 ? 0 : b
+}
+
+console.log(fib(6))
+
+//// space complexity 0(n)
+
+///// Climbing Stairs
+
+function climbStairs(n){
+    let dp = new Array(n+1)
+
+    dp[1] = 1
+    dp[2] = 2
+
+    for(let i = 3; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2]
+    }
+
+    return dp[n]
+}
+
+console.log(climbStairs(5))
+
+/// output 8 ways
+
+
+
+
+
+
+
+
+
+
+
+
+
+

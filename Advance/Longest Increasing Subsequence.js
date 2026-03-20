05e32a90e3cdd9c91bc0e282201668396f05d621
Nawin chander <nawinnapzy289@gmail.com>
2026-03-20 09:34:52 +0530

    add code

--- Advance/Longest Increasing Subsequence.js
+++ Advance/Longest Increasing Subsequence.js
@@ -8,6 +8,35 @@
 // tails[i] = smallest ending element of an increasing subsequence of length i+1
 
 
+function lengthOfLIS(nums) {
+  let tails = [];
 
+  for (let num of nums) {
+    let left = 0;
+    let right = tails.length;
+
+    while (left < right) {
+      let mid = Math.floor((left + right) / 2);
+
+      if (tails[mid] < num) {
+        left = mid + 1;
+      } else {
+        right = mid;
+      }
+    }
+
+    tails[left] = num;
+  }
+
+  return tails.length;
+}
+
+// Example
+console.log(lengthOfLIS([10,9,2,5,3,7,101,18])); // 4
+
+
+// Complexity:
+// Time: O(n log n)
+// Space: O(n)
 
 
